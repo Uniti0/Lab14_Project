8776fe1dc2e9c232a632f8fbd61e841190a9fae6
L1ndis <matvechaynicov2005@gmail.com>
2023-04-26 13:03:00 +0300

    Написал код (Павел)

--- Lab14_Project/Lab14_Project/Program.cs
+++ Lab14_Project/Lab14_Project/Program.cs
@@ -10,7 +10,26 @@ namespace Lab14_Project
     {
         static void Main(string[] args)
         {
-			
+			int[] Sell_price = new int[4];
+            Sell_price[0] = 100;
+            Sell_price[1] = 80;
+            Sell_price[2] = 60;
+            Sell_price[3] = 120;
+
+            double temp = 0;
+            double temp1 = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (Sell_price[i] > temp ) 
+                {
+                    temp = Sell_price[i];
+                    temp1 = i;
+                }  
+
+            }
+            Console.WriteLine("Самый выгодный банк для продажи долларов: " + temp1);
+            Console.ReadKey();
         }
     }
 }
