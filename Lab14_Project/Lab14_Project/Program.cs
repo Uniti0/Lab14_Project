750d51ce645bd1e658289337143d88ed32ea8235
L1NDIS <matvechaynicov2005@gmail.com>
2023-04-26 13:18:02 +0300

    Доработал программный код (Матвей)

--- Lab14_Project/Lab14_Project/Program.cs
+++ Lab14_Project/Lab14_Project/Program.cs
@@ -10,7 +10,38 @@ namespace Lab14_Project
     {
         static void Main(string[] args)
         {
-			
+            string[] banks = { "Сбербанк", "ВТБ", "Газпромбанк", "Альфа-банк", "Тинькофф", "Росбанк", "Уралсиб", "Хоум Кредит", "Райффайзенбанк", "Промсвязьбанк" };
+            int Kol_vo_BANKOV = banks.Length;
+
+            int[] Sell_price = new int[Kol_vo_BANKOV];
+
+            Random rnd = new Random();
+            int Kyrs_Valyt = rnd.Next(59, 121);
+
+            int SamayaVisokayaCena = 0;
+            string LychshiyBankRossii = "";
+
+            Console.WriteLine("Курсы валют:");
+            Console.WriteLine();
+            for (int i = 0; i < Kol_vo_BANKOV; i++)
+            {
+                double randomOtklonenie = rnd.NextDouble() * 0.2 - 0.1; // отклонение в 10%
+                Sell_price[i] = (int)(Kyrs_Valyt * (1 + randomOtklonenie) * 100) / 100; // округляем до 2 знаков после запятой
+                Console.WriteLine(banks[i] + ": " + "(" + Sell_price[i] + ")");
+                if (Sell_price[i] > SamayaVisokayaCena)
+                {
+                    SamayaVisokayaCena = Sell_price[i];
+                    LychshiyBankRossii = banks[i];
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write("Введите сумму в долларах: ");
+            double dollars = double.Parse(Console.ReadLine());
+            double maxRubles = SamayaVisokayaCena * dollars;
+            Console.WriteLine("Самый выгодный банк для продажи долларов: " + LychshiyBankRossii);
+            Console.ReadLine();
+            //Можно дописать покупку доллара, покупка есть
         }
     }
 }
